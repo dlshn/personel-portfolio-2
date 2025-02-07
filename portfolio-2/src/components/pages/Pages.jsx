34e12065ec6d1cefa7b8a50a2ff3b39c4c1f52e0
Dilshan Gamage <randikasandaruwan917@gmail.com>
2025-02-08 00:58:55 +0530

    Navbar

---
+++ portfolio-2/src/components/pages/Pages.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Header } from '../common/Header';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+const pages = () => {
+  return (
+    <>
+    <Router>
+        <Header />
+        <Switch>
+          
+          
+        </Switch>
+    </Router>
+    </>
+  )
+}
+
+export default pages
